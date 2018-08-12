42945c112642d9a614f55aa9d3b84d092f25cadc
chrisziegler <chrisrziegler@gmail.com>
2018-08-11 21:40:36 -0700

    added comments to post.js

--- src/templates/post.js
+++ src/templates/post.js
@@ -3,8 +3,6 @@ import Helmet from 'react-helmet'
 
 // data will be a prop that gets injectd by the GraphQL query
 export default function Template({ data }) {
-  // just some js destructuring
-  // markdownRemark will be reassigned as post
   const { markdownRemark: post } = data
   return (
     <div>
@@ -13,10 +11,8 @@ export default function Template({ data }) {
     </div>
   )
 }
-// the query is looking for a given blogpost
-// it's getting the path
-// then it's finding the markdown file using markdownRemark
-// where the frontmatter. path is equal to the $path
+// query for individual blog posts takes in a graphQL variable
+// uses markdownRemark syntax to return items in html template
 export const postQuery = graphql`
   query BlogPostByPath($path: String!) {
     markdownRemark(frontmatter: { path: { eq: $path } }) {
